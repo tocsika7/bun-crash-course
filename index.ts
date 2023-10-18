accefa350b301e7dd92e81effb10a4e7a663afed
tocsika7 <tocsamanatee@gmail.com>
2023-10-18 19:35:40 +0200

    Created a simple server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 5000, 
+    fetch(req) {
+        return new Response('Hello World');
+    }
+})
+
+console.log(`Listening on port ${server.port}`);
