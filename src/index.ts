4423db91c3cc9754688f6c40d05c2ffb44efb427
tocsika7 <tocsamanatee@gmail.com>
2023-10-18 20:49:38 +0200

    Trying out react

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import fetch_user from "./github_api";
-
-(async () => {
-    const user_data = await fetch_user('tocsika7');
-    document.querySelector('h1').innerHTML = JSON.stringify(user_data);
-})();
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createRoot} from 'react-dom';
+const root = createRoot(document.getElementById('root'));
+
+const App = () => {
+    const [count,setCount] = React.useState(0);
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <h2>Count {count}</h2>
+            <button onClick={ () =>setCount(count + 1)}>Increment </button>
+        </div>
+    )
+}
+
+root.render(<App/>)
